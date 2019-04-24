f78058ff5b2acacfcc4eb257bf01868b773f9995
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-04-25 09:40:50 +1200

    update scripts

--- 02-install-cio.sh
+++ 02-install-cio.sh
@@ -3,3 +3,5 @@
 vagrant ssh storidge-1 --no-tty -c "curl -fsSL ftp://download.storidge.com/pub/ce/cio-ce | sudo bash;"
 vagrant ssh storidge-2 --no-tty -c "curl -fsSL ftp://download.storidge.com/pub/ce/cio-ce | sudo bash;"
 vagrant ssh storidge-3 --no-tty -c "curl -fsSL ftp://download.storidge.com/pub/ce/cio-ce | sudo bash;"
+
+exit 0
